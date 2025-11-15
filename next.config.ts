eda3a74a384e07d942a80059c5fda6d7934f81d1
thathaji kotari <thathajikotari3@gmail.com>
2025-11-15 10:31:23 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",   // apply to all API routes
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://delightful-custard-83a956.netlify.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
